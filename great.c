32ab100bf21afa0bb5ab70f6d93efa8feba23590
sivasree2007 <sivasree08092007@gmail.com>
2026-09-08 15:32:27 +0530

    Add files via upload

---
+++ great.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int n1,n2,n3,sum;
+if(n1>n2&&n1>n3)
+printf("The Greatest number is ",n1);
+else if (n2>n1&&n2>n3);
+printf("The Greatest number is ",n2);
+ifelse
+printf("The Greatest number is ",n3);
+return 0;
+}
